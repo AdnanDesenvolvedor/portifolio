2bda139c3b33abf0d32c3086311c0d4a8988e812
Adnan Alixandre <83788477+AdnanDesenvolvedor@users.noreply.github.com>
2022-11-23 11:16:16 -0300

    commit inicial
    primeiro commit do site

---
+++ index.php
@@ -0,0 +1,284 @@
+<!DOCTYPE html>
+<html lang="PT-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="estilo/estilo.css" media="screen" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <title src="midia/icone_genio1 (1).ico">Gênio digital </title>
+</head>
+ <style>
+   .corprimaria{
+     background-color:black;
+   }
+   .textopreto{
+     color:black;
+     
+   }
+   .textobranco{
+     color:grey;
+     text-align:justify;
+   }
+    footer{
+      height:170px;
+      background-color:black; 
+    }
+    a{
+      margin-left: 5px; 
+    }
+    h4{
+      color:white;
+      margin-top: 100px;
+      margin-left: 8px;
+      font-size: 15px;
+    }
+    h1{
+      color:white;
+      text-align: center;
+      font-size: 20px;
+    }
+    .icone{
+      width: 300px;
+    }
+    .icones{
+       margin-top: 100px;
+       margin-left: 8px;
+    }
+    .cabecalho{
+      background-color: black;
+    }
+    .iconeprincipal{
+       width: 80px;
+       height: 80px;
+    }
+    .titulo{
+      color:white;
+      font-style: oblique	
+    }
+    .descricao{
+      color:grey;
+    }
+    .projetos{
+      font-style: oblique	;
+    }
+    .sobre{
+       color:grey;
+       font-style: oblique;
+    }
+    .serviços{
+      font-style: oblique;	
+    }
+    .imagens{
+      border-radius: 50px;
+      width: 60px;
+    }
+    .texto{
+      text-align:justify;
+    }
+     
+ </style>
+<body class="text-center">
+<div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column cabecalho">
+  <header class="masthead mb-auto">
+     <div class="inner">
+       <h3 class="masthead-brand titulo" >Gênio digital</h3>
+       <img src="midia/icone_genio1 (1).ico" class="rounded mx-auto d-block imagens">
+        <nav class="nav nav-masthead justify-content-center">
+         <a class="nav-link active" href="#projetos">Projetos</a>
+         <a class="nav-link" href="#sobre-min">Sobre</a>
+         <a class="nav-link" href="#featured-3">Serviços</a>
+      </nav>
+     </div>
+  </header>
+  <main role="main" class="inner cover">
+    <h1 class="cover-heading">Desenvolvedor de sistemas </h1>
+    <p class="lead descricao">Bem vindo ao meu portifólio! Sou formado em análise e desenvolvimento de sistemas, faço criação de sistema web programas para computadores pessoais e empresariais</p>
+    <p class="lead">
+      <a href="#contato" class="btn btn-lg btn-primary">Saiba mais</a>
+    </p>
+  </main>
+</div>
+<!-- Principal JavaScript do Bootstrap
+================================================== -->
+<!-- Foi colocado no final para a página carregar mais rápido -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+<script src="../../assets/js/vendor/popper.min.js"></script>
+<script src="../../dist/js/bootstrap.min.js"></script>
+  <main role="main">
+   <div class="album py-5 bg-light" id="projetos">
+   <h2 class="pb-2 border-bottom projetos"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Projetos</font></font></h2>
+   <br><br>
+   <div class="container">
+    <div class="row">
+      <div class="col-md-4">
+        <div class="card mb-4 box-shadow">
+          <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail" alt="Thumbnail [100%x225]" style="height: 225px; width: 100%; display: block;" src="midia/em_breve.png" data-holder-rendered="true">
+          <div class="card-body">
+            <p class="card-text">Esse projeto foi ultilizado usando as tecnologias</p>
+            <ul>
+              <li>Em breve</li>
+              <li>Em breve</li>
+              <li>Em breve</li>
+            </ul>
+            <div class="d-flex justify-content-between align-items-center">
+              <div class="btn-group">
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="card mb-4 box-shadow">
+          <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail" alt="Thumbnail [100%x225]" src="midia/em_breve.png" data-holder-rendered="true" style="height: 225px; width: 100%; display: block;">
+          <div class="card-body">
+            <p class="card-text">Esse sistema foi ultilizado usando as tecnologias</p>
+            <ul>
+              <li>Em breve</li>
+              <li>Em breve</li>
+              <li>Em breve</li>
+            </ul>
+            <div class="d-flex justify-content-between align-items-center">
+              <div class="btn-group">
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="card mb-4 box-shadow">
+          <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail" alt="Thumbnail [100%x225]" src="midia/em_breve.png" data-holder-rendered="true" style="height: 225px; width: 100%; display: block;">
+          <div class="card-body">
+            <p class="card-text">Esse projeto foi feito usando as melhores tecnologias</p>
+            <ul>
+              <li>Em breve</li>
+              <li>Em breve</li>
+              <li>Em breve</li>
+            </ul>
+            <div class="d-flex justify-content-between align-items-center">
+              <div class="btn-group">     
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+</main>
+<!--sobre mim-->
+<div class=""id="sobre-min">
+  <br>
+  <section class="jumbotron text-center corprimaria"id="sobre-min" >
+        <div class="container">
+           <h2 class="jumbotron heading sobre">Sobre </h2>
+           <p class="lead textobranco">Sou formado em análise e desenvolvimento de sistemas pela universidade centro universitario Leonardo da Vinci ao longo da minha carreira tive a oportunidade de 
+            de estagiar como desenvolvedor de sistemas web junior usando as tecnologias java, php, e banco de dados MySql com experiencia em desenvolver sistemas web.
+           </p>
+             <br>
+        </div>
+  </section>
+</div>
+<br><br>
+<div class="row mb-2">
+        <div class="col-md-6">
+          <div class="card flex-md-row mb-4 shadow-sm h-md-250">
+            <div class="card-body d-flex flex-column align-items-start">
+              <strong class="d-inline-block mb-2 text-primary">figma</strong>
+              <h3 class="mb-0">
+                <h1 class="text-dark" href="#">Layout a partir do figma</h1>
+              </h3>
+              <div class="mb-1 text-muted">18 de outubro</div>
+              <p class="texto">Editor graficos usado pra criar interfaces graficas do jeito que o cliente realmente deseja, que é plataforma que é usada para a contrução de projetos de desing de interfaces.</p>
+            </div>
+            <img class="card-img-right flex-auto d-none d-lg-block" data-src="holder.js/200x250?theme=thumb" alt="Thumbnail [200x250]" style="width: 200px; height: 250px;" src="midia/figma.png">
+          </div>
+        </div>
+        <div class="col-md-6">
+          <div class="card flex-md-row mb-4 shadow-sm h-md-250">
+            <div class="card-body d-flex flex-column align-items-start">
+              <strong class="d-inline-block mb-2 text-success">Design</strong>
+              <h3 class="mb-0">
+                <h1 class="text-dark" href="#">Sistemas responsivos</h1>
+              </h3>
+              <div class="mb-1 text-muted">18 de outubro</div>
+              <p class="texto">Os sistemas feitos para ser exibidos em qualquer tipo de dispositivos como telas  de dispositivos como telas de celulares, tablets, e computadores de diferentes tamanhos que facilita na visualização do seu dispositivo</p>
+            </div>
+            <img class="card-img-right flex-auto d-none d-lg-block" data-src="holder.js/200x250?theme=thumb" alt="Thumbnail [200x250]" src="midia/sistema_resposivo.jpg" data-holder-rendered="true" style="width: 200px; height: 250px;">
+          </div>
+        </div>
+      </div>
+ <!---serviços-->
+ <div class="container px-4 py-5" id="featured-3">
+    <h2 class="pb-2 border-bottom serviços"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Serviços</font></font></h2>
+    <div class="row g-4 py-5 row-cols-1 row-cols-lg-3">
+      <div class="feature col">
+        <div class="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3">
+        <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="" class="bi bi-tools" viewBox="0 0 16 16">
+         <path d="M1 0 0 1l2.2 3.081a1 1 0 0 0 .815.419h.07a1 1 0 0 1 .708.293l2.675 2.675-2.617 2.654A3.003 3.003 0 0 0 0 13a3 3 0 1 0 5.878-.851l2.654-2.617.968.968-.305.914a1 1 0 0 0 .242 1.023l3.27 3.27a.997.997 0 0 0 1.414 0l1.586-1.586a.997.997 0 0 0 0-1.414l-3.27-3.27a1 1 0 0 0-1.023-.242L10.5 9.5l-.96-.96 2.68-2.643A3.005 3.005 0 0 0 16 3c0-.269-.035-.53-.102-.777l-2.14 2.141L12 4l-.364-1.757L13.777.102a3 3 0 0 0-3.675 3.68L7.462 6.46 4.793 3.793a1 1 0 0 1-.293-.707v-.071a1 1 0 0 0-.419-.814L1 0Zm9.646 10.646a.5.5 0 0 1 .708 0l2.914 2.915a.5.5 0 0 1-.707.707l-2.915-2.914a.5.5 0 0 1 0-.708ZM3 11l.471.242.529.026.287.445.445.287.026.529L5 13l-.242.471-.026.529-.445.287-.287.445-.529.026L3 15l-.471-.242L2 14.732l-.287-.445L1.268 14l-.026-.529L1 13l.242-.471.026-.529.445-.287.287-.445.529-.026L3 11Z"/>
+        </svg>
+        </div>
+        <h3 class="fs-2"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Manutenção</font></font></h3>
+        <p class="texto">É um processo que consiste em fazer mudanças no sistema do cliente  durante a vida util, e de acordo com a necessidade de atualizações ou mudanças a partir do que o cliente deseja</p>
+          </font></font><svg class="bi" width="1em" height="1em"><use xlink:href="#chevron-right"></use></svg>
+      </div>
+      <div class="feature col">
+        <div class="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3">
+        <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="" class="bi bi-code-slash" viewBox="0 0 16 16">
+       <path d="M10.478 1.647a.5.5 0 1 0-.956-.294l-4 13a.5.5 0 0 0 .956.294l4-13zM4.854 4.146a.5.5 0 0 1 0 .708L1.707 8l3.147 3.146a.5.5 0 0 1-.708.708l-3.5-3.5a.5.5 0 0 1 0-.708l3.5-3.5a.5.5 0 0 1 .708 0zm6.292 0a.5.5 0 0 0 0 .708L14.293 8l-3.147 3.146a.5.5 0 0 0 .708.708l3.5-3.5a.5.5 0 0 0 0-.708l-3.5-3.5a.5.5 0 0 0-.708 0z"/>
+       </svg>
+        </div>
+        <h3 class="fs-2"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Código fonte</font></font></h3>
+        <p class="texto">Desenvolvimento de projetos em forma digital com as praticas de programação.</p>
+      </div>
+      <div class="feature col">
+        <div class="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3">
+        <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="" class="bi bi-calendar3" viewBox="0 0 16 16">
+        <path d="M14 0H2a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zM1 3.857C1 3.384 1.448 3 2 3h12c.552 0 1 .384 1 .857v10.286c0 .473-.448.857-1 .857H2c-.552 0-1-.384-1-.857V3.857z"/>
+        <path d="M6.5 7a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm-9 3a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm-9 3a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"/>
+        </svg>
+        </div>
+        <h3 class="fs-2"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Prazo de entrega</font></font></h3>
+        <p class="texto">Os sistemas desenvolvidos é estipulado a partir de como os sistema é pedido,com a data de entrega o sistema tem o melhor redimento e são concluidos sem atrasos </p>
+      </div>
+    </div>
+  </div>
+ <!----contato--->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+<div class="icones" id="contato">
+          <a href=" https://api.whatsapp.com/send?phone=55+74999594347&text= Olá, o que podemos ajudar?."><svg xmlns="http://www.w3.org/2000/svg" width="16" height="50" fill="currentColor" class="bi bi-whatsapp" viewBox="0 0 16 16">
+          <path d="M13.601 2.326A7.854 7.854 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.933 7.933 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.898 7.898 0 0 0 13.6 2.326zM7.994 14.521a6.573 6.573 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.557 6.557 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592zm3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.729.729 0 0 0-.529.247c-.182.198-.691.677-.691 1.654 0 .977.71 1.916.81 2.049.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232z"/>
+          </svg></a>
+          <a href="https://www.linkedin.com/in/adnan-alixandre-56861a1b5"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="50" fill="currentColor" class="bi bi-linkedin" viewBox="0 0 16 16">
+          <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z"/>
+          </svg></a> 
+          <a href="mailto:geniodigitalconsultoria@gmail.com"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope" viewBox="0 0 16 16">
+          <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z"/>
+          </svg></a>
+          <br>
+          <h10>2022-2023 Gênio digital - ©Todos os direitos reservados</h10>
+      </div> 
+     <svg
+          class="waves"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 1440 320"
+        >
+          <path
+            class="wave-1"
+            fill="#363636"
+            fill-opacity="1"
+            d="M0,64L21.8,96C43.6,128,87,192,131,229.3C174.5,267,218,277,262,234.7C305.5,192,349,96,393,48C436.4,0,480,0,524,53.3C567.3,107,611,213,655,224C698.2,235,742,149,785,112C829.1,75,873,85,916,90.7C960,96,1004,96,1047,122.7C1090.9,149,1135,203,1178,197.3C1221.8,192,1265,128,1309,133.3C1352.7,139,1396,213,1418,250.7L1440,288L1440,320L1418.2,320C1396.4,320,1353,320,1309,320C1265.5,320,1222,320,1178,320C1134.5,320,1091,320,1047,320C1003.6,320,960,320,916,320C872.7,320,829,320,785,320C741.8,320,698,320,655,320C610.9,320,567,320,524,320C480,320,436,320,393,320C349.1,320,305,320,262,320C218.2,320,175,320,131,320C87.3,320,44,320,22,320L0,320Z"
+          ></path>
+          <path
+            class="wave-2"
+            fill="#1C1C1C"
+            fill-opacity="1"
+            d="M0,256L21.8,245.3C43.6,235,87,213,131,176C174.5,139,218,85,262,80C305.5,75,349,117,393,133.3C436.4,149,480,139,524,122.7C567.3,107,611,85,655,85.3C698.2,85,742,107,785,128C829.1,149,873,171,916,154.7C960,139,1004,85,1047,101.3C1090.9,117,1135,203,1178,202.7C1221.8,203,1265,117,1309,106.7C1352.7,96,1396,160,1418,192L1440,224L1440,320L1418.2,320C1396.4,320,1353,320,1309,320C1265.5,320,1222,320,1178,320C1134.5,320,1091,320,1047,320C1003.6,320,960,320,916,320C872.7,320,829,320,785,320C741.8,320,698,320,655,320C610.9,320,567,320,524,320C480,320,436,320,393,320C349.1,320,305,320,262,320C218.2,320,175,320,131,320C87.3,320,44,320,22,320L0,320Z"
+          ></path>
+          <path
+            class="wave-3"
+            fill="black"
+            fill-opacity="1"
+            d="M0,256L21.8,213.3C43.6,171,87,85,131,69.3C174.5,53,218,107,262,117.3C305.5,128,349,96,393,106.7C436.4,117,480,171,524,192C567.3,213,611,203,655,176C698.2,149,742,107,785,112C829.1,117,873,171,916,213.3C960,256,1004,288,1047,277.3C1090.9,267,1135,213,1178,170.7C1221.8,128,1265,96,1309,101.3C1352.7,107,1396,149,1418,170.7L1440,192L1440,320L1418.2,320C1396.4,320,1353,320,1309,320C1265.5,320,1222,320,1178,320C1134.5,320,1091,320,1047,320C1003.6,320,960,320,916,320C872.7,320,829,320,785,320C741.8,320,698,320,655,320C610.9,320,567,320,524,320C480,320,436,320,393,320C349.1,320,305,320,262,320C218.2,320,175,320,131,320C87.3,320,44,320,22,320L0,320Z"
+          ></path>
+        </svg>
+</body>
+</html>
